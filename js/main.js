e1ddd8ebe7ba7bead80100cce3e762ccc1d681bd
EddyDiazS <eddya.diazs@konradlorenz.edu.co>
2024-04-04 10:38:20 -0500

    Borrar ciertas notas

--- js/main.js
+++ js/main.js
@@ -5,22 +5,22 @@
                 url: "https://pokeapi.co/api/v2/pokemon/" + pokemonName,
                 contentType: "application/json",
                 success: function (data) {
-                    // Mostrar la información del Pokémon si se encontró
+
                     mostrarInformacionPokemon(data);
                 },
                 error: function (xhr, status, error) {
-                    // Mostrar mensaje de error si el Pokémon no se encuentra
+
                     mostrarError("ERROR 404 POKEMON NO ENCONTRADO");
                 }
             });
         });
 
         $("#limpiar").on("click", function() {
-            // Limpiar la información
+
             $(".pokemon-info").hide();
             $(".imagen").empty();
             $(".stats-container").empty();
-            $(".info-container").empty(); // Limpiar la información de nombre y clase
+            $(".info-container").empty(); 
             $(".error-container").hide();
             $(".error-message").hide();
         });
@@ -45,43 +45,43 @@
             water: "water"
         };
         function mostrarInformacionPokemon(data) {
-            $(".error-message").hide(); // Ocultar el mensaje de error si está visible
+            $(".error-message").hide(); 
             var tipoPokemon = obtenerTipoPokemon(data);
             if (tipoPokemon !== null) {
-                $(".pokemon-info").show(); // Mostrar el contenedor de información
+                $(".pokemon-info").show();
                 asignarClaseColor(tipoPokemon);
                 $(".imagen").html(`<img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}" class="pokemon-img">`);
                 mostrarNombre(data.name);
                 mostrarClase(tipoPokemon);
-                mostrarTipoSymbol(tipoPokemon); // Agregar símbolo del tipo
+                mostrarTipoSymbol(tipoPokemon); 
                 mostrarEstadisticas(data.stats);
             } else {
-                mostrarError("Error 404: Pokémon no encontrado."); // Mensaje de error personalizado
+                mostrarError("Error 404: Pokémon no encontrado."); 
             }
         }
         
         function obtenerTipoPokemon(data) {
-            // Verificar si existen datos de tipo
+           
             if (data.types && data.types.length > 0) {
                 // Obtener el primer tipo de Pokémon de los datos recibidos
                 return data.types[0].type.name;
             }
-            return null; // Si no se encuentra ningún tipo, devolver null
+            return null; 
         }
         
         function mostrarTipoSymbol(tipoPokemon) {
-            var tipoSymbol = $('<div class="type-symbol"></div>'); // Crear un nuevo elemento div para el símbolo
+            var tipoSymbol = $('<div class="type-symbol"></div>'); 
             tipoSymbol.addClass(tipoColorMap[tipoPokemon]); // Agregar la clase correspondiente al tipo de Pokémon
-            $(".imagen").append(tipoSymbol); // Agregar el símbolo al contenedor de la imagen
+            $(".imagen").append(tipoSymbol); 
         }
         
         function asignarClaseColor(tipoPokemon) {
             console.log("Tipo de Pokémon:", tipoPokemon);
         
-            // Limpiar todas las clases de color
+           
             $(".pokemon-info").removeClass("bug dark dragon electric fairy fighting fire flying ghost grass ground ice normal poison psychic rock steel water");
             
-            // Asignar la clase de color correspondiente al tipo de Pokémon
+            
             switch (tipoPokemon) {
                 case "bug":
                     console.log("Agregando clase: bug");
@@ -156,12 +156,12 @@
                     $(".pokemon-info").addClass("water");
                     break;
                 default:
-                    // Si no se encuentra el tipo, no se asigna ninguna clase de color
+                    
                     console.log("Tipo de Pokémon no válido:", tipoPokemon);
                     break;
             }
         
-            // Imprimir las clases finales
+            
             console.log("Clases finales:", $(".pokemon-info").attr("class"));
         }
         
@@ -206,10 +206,10 @@
                 
                 var statBar = $('<div class="stat-bar"></div>');
                 var statFill = $('<div class="stat-fill"></div>').css('width', (stat.base_stat / 2) + '%');
-                var statLabel = $('<span class="stat-label">' + statName + '</span>'); // Eliminamos los dos puntos
-                var statValue = $('<span class="stat-value">' + stat.base_stat + '</span>').css('font-size', '18px'); // Ajustamos el tamaño de fuente
+                var statLabel = $('<span class="stat-label">' + statName + '</span>'); 
+                var statValue = $('<span class="stat-value">' + stat.base_stat + '</span>').css('font-size', '18px'); 
                 
-                statBar.addClass("stat-" + stat.stat.name); // Agregar la clase correspondiente al tipo de estadística
+                statBar.addClass("stat-" + stat.stat.name); 
                 statBar.append(statLabel);
                 statBar.append(statFill);
                 statBar.append(statValue);
@@ -240,7 +240,7 @@
             }
 
             reconocimiento.onresult = function (event) {
-                const current = event.resultIndex; // Corrección aquí
+                const current = event.resultIndex; 
 
                 const transcribir = event.results[current][0].transcript;
 
@@ -254,10 +254,10 @@
 
             
             function buscarPokemon() {
-            // Obtener el valor transcrito
+            
             const transcripcion = document.getElementById("txt_buscador").value;
         
-            // Si la transcripción no está vacía, buscar el Pokémon
+            
                 if (transcripcion) {
             $("#buscar").click();
                 }
